6c8af462a39f4e2b5d7e8e9d8d68527785d7c408
Chris Howard <Chris.Howard@sikich.com>
2020-03-19 09:56:22 -0500

    Update how server is kicked off

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn runserver --log-file -
+web: gunicorn app:create_app() --log-file -
---
+++ app.py
@@ -0,0 +1,13 @@
+from os import environ
+from SalesforceUtilityTools import app
+
+def create_app():
+    #HOST = environ.get('SERVER_HOST', 'localhost')
+    #try:
+    #    PORT = int(environ.get('SERVER_PORT', '5555'))
+    #except ValueError:
+    #    PORT = 5555
+    #return app.run(HOST, PORT)
+    return app.run()
+
+my_app = create_app()
