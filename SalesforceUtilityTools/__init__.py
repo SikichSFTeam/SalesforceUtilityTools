28b2b29c47557d56261d587ac424c0afa8298a7c
Chris Howard <Chris.Howard@sikich.com>
2020-03-20 12:26:56 -0500

    Set default values

--- SalesforceUtilityTools/__init__.py
+++ SalesforceUtilityTools/__init__.py
@@ -4,7 +4,7 @@ The flask application package.
 import logging, sys
 from flask import Flask
 app = Flask(__name__)
-logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 from . import views
 from SalesforceUtilityTools.controllers.tools import *
