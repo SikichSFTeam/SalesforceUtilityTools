7e5368146ac70439c011ef60715a880399031344
Chris Howard <Chris.Howard@sikich.com>
2020-03-24 14:39:10 -0500

    Correct SF OAuth URL

--- SalesforceUtilityTools/views.py
+++ SalesforceUtilityTools/views.py
@@ -9,40 +9,60 @@ from flask import render_template
 from flask import request
 from SalesforceUtilityTools import app
 from SalesforceUtilityTools.controllers import *
-import sys, logging
+import sys, logging, yaml
+
+def _init():
+    with open('config.yaml','r') as cfgFile:
+        config = yaml.load(cfgFile, Loader=yaml.SafeLoader)
+
+    viewData = {
+            'title': 'Sikich Utility Tools',
+            'year': datetime.now().year,
+            'sfClientId': config['salesforce']['consumerKey'],
+            'sfClientSecret': config['salesforce']['consumerSecret'],
+        }
+
+    return viewData
 
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
+    viewData = _init()
     return render_template(
         'index.html',
-        title='Home Page',
-        year=datetime.now().year,
+        viewData=viewData,
     )
 
 @app.route('/contact')
 def contact():
     """Renders the contact page."""
+    viewData = _init()
+    viewData['title'] = 'Contact'
+
     return render_template(
         'contact.html',
-        title='Contact',
-        year=datetime.now().year,
+        viewData=viewData,
         message='Your contact page.'
     )
 
 @app.route('/about')
 def about():
     """Renders the about page."""
+    viewData = _init()
+    viewData['title']='About'
+
     return render_template(
         'about.html',
-        title='About',
-        year=datetime.now().year,
+        viewData=viewData,
         message='Your application description page.'
     )
 
 @app.route('/oathLogin', methods=['POST', 'GET'])
 def oathLogin():
+    viewData = _init()
+    viewData['title']='About'
+
     cntlr = oauthLoginCtrl()
     responseData = cntlr.processRequest()
 
@@ -52,6 +72,9 @@ def oathLogin():
 
 @app.route('/tools/cleanHtml', methods=['POST', 'GET'])
 def tools_cleanHtml():
+    viewData = _init()
+    viewData['title']='Clean Html'
+
     cntlr = tools.cleanHtml()
     responseData = cntlr.processRequest()
 
@@ -60,13 +83,15 @@ def tools_cleanHtml():
 
     return render_template(
         'tools/cleanHtml.html',
-        title='Clean Html',
-        year=datetime.now().year,
+        viewData=viewData,
         responseData=responseData,
     )
 
 @app.route('/tools/createTemplate', methods=['POST', 'GET'])
 def tools_createTemplate():
+    viewData = _init()
+    viewData['title']='Create Template'
+
     cntlr = tools.createTemplate()
     responseData = cntlr.processRequest()
 
@@ -75,7 +100,6 @@ def tools_createTemplate():
 
     return render_template(
         'tools/createTemplate.html',
-        title='Create Template',
-        year=datetime.now().year,
+        viewData=viewData,
         responseData=responseData,
     )
