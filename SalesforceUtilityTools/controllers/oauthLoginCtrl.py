59f55812fa48ff32803f977dbac20295ec8b5844
Chris Howard <Chris.Howard@sikich.com>
2020-03-25 18:45:46 -0500

    Missed updating a spot

--- SalesforceUtilityTools/controllers/oauthLoginCtrl.py
+++ SalesforceUtilityTools/controllers/oauthLoginCtrl.py
@@ -36,10 +36,10 @@ class oauthLoginCtrl(masterController):
         flask.session.modified = True
         
         # Create hook to set cookie value
-        @flask.after_this_request
-        def add_header(response):
-            response.set_cookie('SalesforceAuthCode', value=authCode)
-            response.set_cookie('SalesforceHostname', value=host)
-            return response
+        #@flask.after_this_request
+        #def add_header(response):
+        #    response.set_cookie('SalesforceAuthCode', value=authCode)
+        #    response.set_cookie('SalesforceInstance', value=instance)
+        #    return response
 
         return self.responseData
